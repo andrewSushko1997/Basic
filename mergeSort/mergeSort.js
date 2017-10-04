fa5a9cec2331843656eae420e7643115cffe89e4
andrewSushko1997 <andrewklopz@gmail.com>
2017-10-05 01:11:19 +0300

    Add quickSort method

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,4 +1,4 @@
-function ArrayMethod (arrayForSort) {
+function ArrayMethods (arrayForSort) {
 	this.arrayForSort = arrayForSort;
 	this.mergeSort = function sortArrays(arraysForSort) {
 		if(arraysForSort === undefined) arraysForSort = this.arrayForSort;
@@ -11,6 +11,25 @@ function ArrayMethod (arrayForSort) {
 				
 
 		return merge(sortArrays(left), sortArrays(right));
+	}
+
+	this.quickSort = function sortArrays(arraysForSort) {
+		if(arraysForSort === undefined) arraysForSort = this.arrayForSort;
+		if(arraysForSort.length < 2) return arraysForSort;
+
+		var supportingElement = arraysForSort[0],
+				left = [],
+				right = [];
+		for(var i = 1; i < arraysForSort.length; i++) {
+			if(supportingElement >= arraysForSort[i]) {
+				left.push(arraysForSort[i]);
+			} else {
+				right.push(arraysForSort[i]);
+			}
+		}
+
+		return sortArrays(left).concat(supportingElement, sortArrays(right));
+
 	}
 	function merge(left, right) {
 		var result = [];
@@ -26,5 +45,13 @@ function ArrayMethod (arrayForSort) {
 	}
 }
 
-var arr = new ArrayMethod([2,5,4,8,2,9,1, 17, 14, 2, 15, 29, 7, 4, 3, 7, 11, 28, 14, 3]);
+var arr = new ArrayMethods([2,5,4,8,2,9,1, 17, 14, 2, 15, 29, 7, 4, 3, 7, 11, 28, 14, 3]);
 console.log(arr.mergeSort());
+
+console.log(arr.arrayForSort);
+
+console.log(arr.quickSort());
+
+var arr1 = new ArrayMethods([2,5,4,8,2,9,1, 17, 14, 2, 15, 29, 7, 4, 3, 7, 11, 28, 14, 3]);
+
+console.log(arr1.quickSort());
